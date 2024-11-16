8b7cbc235993b353c700141231f1dec08a282ced
bmestref <tolo201311@gmail.com>
2024-11-16 04:02:09 +0100

    Update OptiWatt.py

--- OptiWatt/OptiWatt.py
+++ OptiWatt/OptiWatt.py
@@ -84,6 +84,54 @@ def maximize_cost(precios, produccion, graph = False, S_max = 15000,
         
         return(problem.value, v.value, s.value, b_in.value, b_out.value)
 
-    
+def maximize_cost_penal(precios, produccion_real, produccion, penal, S_max = 3600, S_min = 0, B_max = 1800, V_max = 4000, S_init = 1080):
+    # Variables
+    real = maximize_cost(precios, produccion_real, S_max = S_max, S_min = S_min, B_max = B_max, V_max = V_max, S_init = S_init)
+    v_real = real[1]
+    total_real = real[0]
+    n = 24  # Número de horas
+    v = cp.Variable(n)         # Energía vendida al mercado por hora
+    b_in = cp.Variable(n)      # Energía que entra a la batería por hora
+    b_out = cp.Variable(n)     # Energía que sale de la batería por hora
+    s = cp.Variable(n)     # Estado de carga de la batería (incluye estado inicial)
+    carga_flag = cp.Variable(n, boolean=True)
+    # Restricciones
+    constraints = []
+        
+    # Inicializar el estado de la batería
+    constraints.append(s[0] == S_init)
+    constraints.append(s[-1] == S_init)
+    # Restricciones por hora
+    for t in range(n):
+        def positive_value(expr):
+            try:
+                cp.sqrt(expr)
+                return expr
+            except ValueError:
+                return -expr
+            
+        # Límites de almacenamiento de la batería
+        constraints.append(0 <= s[t])
+        constraints.append(s[t] <= S_max)
+        # Límites de carga/descarga
+        constraints.append(s[t] == s[t-1] + b_in[t] - b_out[t])
+        constraints.append(0 <= b_in[t])
+        constraints.append(b_in[t] <= B_max * carga_flag[t])
+        constraints.append(0 <= b_out[t])
+        constraints.append(b_out[t] <= B_max * (1 - carga_flag[t]))
+        # Límite de venta
+        constraints.append(0 <= v[t])
+        constraints.append(v[t] <= V_max)
+        constraints.append(v[t] == produccion[t] + (s[t-1] - s[t]) - positive_value(penal[t]*(v_real[t] - v[t])))
+        if produccion[t] == 0:
+            constraints.append(b_in[t] == 0)
+        
+    # Función objetivo: maximizar beneficios
+    objective = cp.Maximize(cp.sum(cp.multiply(precios, v)))
+
+    # Resolver el problema
+    problem = cp.Problem(objective, constraints)
+    problem.solve(solver = cp.GUROBI)
+    return(problem.value, v.value, s.value, b_in.value, b_out.value, v_real, v_real - v.value, total_real, v_real)
 
 
